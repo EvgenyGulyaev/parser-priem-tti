37321dd10d5f850edcb1b05bac1746dc727389e7
Evgeny <wardercompany@gmail.com>
2020-07-31 17:53:15 +0300

    Update for masters

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const groups = {
   '28.03.02': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP28.03.026200OSS',
   '38.03.04': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGUE38.03.046200OSS',
   '38.03.05': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGUE38.03.056200OSS',
+
   '09.06.01': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT09.06.017200OSS',
   '03.06.01': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP03.06.017200OSS',
   '10.06.01': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT10.06.017200OSS',
@@ -30,6 +31,24 @@ const groups = {
   '15.06.01': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT15.06.017200OSS',
   '24.06.01': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT24.06.017200OSS',
   '27.06.01': 'https://sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT27.06.017200OSS',
+
+  '28.04.01': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP28.04.0168038OSS',
+  '27.04.04': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT27.04.0468035OSS',
+  '27.04.03': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT27.04.0368034OSS',
+  '15.04.04': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT15.04.046802OSS',
+  '15.04.06': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT15.04.066801OSS',
+  '20.04.01': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP20.04.0168029OSS',
+  '13.04.02': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT13.04.026801OSS',
+  '12.04.04': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP12.04.046801OSS',
+  '12.04.01': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT12.04.0168028OSS',
+  '11.04.04': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP11.04.046802OSS',
+  '11.04.03': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGEP11.04.0368099OSS',
+  '11.04.02': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT11.04.0268027OSS',
+  '11.04.01': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGRT11.04.016801OSS',
+  '09.04.04': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT09.04.0468026OSS',
+  '09.04.03': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT09.04.036809OSS',
+  '09.04.01': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT09.04.016800OSS',
+  '01.04.02': 'https://www.sfedu.ru/php_j/abitur/show.php?finance=b&list=TGKT01.04.026809OSS',
 };
 
 const aspirantura = [
@@ -43,6 +62,26 @@ const aspirantura = [
   '27.06.01',
 ];
 
+const master = [
+  '28.04.01',
+  '27.04.04',
+  '27.04.03',
+  '15.04.04',
+  '15.04.06',
+  '20.04.01',
+  '13.04.02',
+  '12.04.04',
+  '12.04.01',
+  '11.04.04',
+  '11.04.03',
+  '11.04.02',
+  '11.04.01',
+  '09.04.04',
+  '09.04.03',
+  '09.04.01',
+  '01.04.02',
+];
+
 class Parser {
   async getStudents(group = '09.03.04') {
     const { data } = await axios.get(groups[group]);
@@ -50,12 +89,12 @@ class Parser {
     let tables = $('table');
     const trs = tables[tables.length - 1].children[1].children.slice(13);
     return trs.map((el) => {
-      const valueIndex = aspirantura.includes(group) ? 28 : 26;
+      const valueIndex = aspirantura.includes(group) ? 28 : master.includes(group) ? 14 : 26;
       const scoreIndex = aspirantura.includes(group) ? 10 : 8;
       return {
         'user': $(el.children[2]).text(),
-        'value': $(el.children[valueIndex]).text(), // Для аспирантов это 28
-        'score': $(el.children[scoreIndex]).text()  //  Для аспирантов 10 
+        'value': $(el.children[valueIndex]).text(), // Для аспирантов это 28 а магистры это 14
+        'score': $(el.children[scoreIndex]).text()  //  Для аспирантов 10
       }
     })
     .filter(({ value } = {}) => value.includes(`1.${group} (о, ГБ);`) || value.includes(`1.${group} (о, ЦП);`))
